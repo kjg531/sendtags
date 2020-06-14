c06a36836042254742249292419b5e8594f04f98
Unknown <kennygabbara@gmail.com>
2020-06-14 15:32:53 -0700

    add more test cases for cleaner functions

--- src/helpers/cleanConfig.test.js
+++ src/helpers/cleanConfig.test.js
@@ -1,6 +1,14 @@
 import { cleanConfig } from './cleanConfig';
+
 test('cleans config of curly quotes', () => {
   expect(cleanConfig('{“Spiderman”: [“hero”, “tough”, “smart”, “tall”]}')).toBe(
     '{"Spiderman": ["hero", "tough", "smart", "tall"]}',
+  );
+  expect(
+    cleanConfig(
+      '{“Human Torch”: [“hero”, “mutant”, “tough”, “dumb”, “tall”], “Spiderman”: [“hero”, “tough”, “smart”, “tall”], “Kyle”: [“human”, “weak”, “smart”, “short”], “JonJon”: [“human”, “strong”, “smart”, “tall”, “weird”]}',
+    ),
+  ).toBe(
+    '{"Human Torch": ["hero", "mutant", "tough", "dumb", "tall"], "Spiderman": ["hero", "tough", "smart", "tall"], "Kyle": ["human", "weak", "smart", "short"], "JonJon": ["human", "strong", "smart", "tall", "weird"]}',
   );
 });
--- src/helpers/cleanSendTo.test.js
+++ src/helpers/cleanSendTo.test.js
@@ -1,4 +1,10 @@
 import { cleanSendTo } from './cleanSendTo';
 test('cleans config of curly quotes', () => {
   expect(cleanSendTo('human,tall,weak')).toStrictEqual(['human', 'tall', 'weak']);
+  expect(cleanSendTo('human,tall,weak,strong')).toStrictEqual([
+    'human',
+    'tall',
+    'weak',
+    'strong',
+  ]);
 });
