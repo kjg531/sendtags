7b929db2945db691c59663b2964004ebf1ec07f2
Unknown <kennygabbara@gmail.com>
2020-06-14 15:27:04 -0700

    add more tests for determineRcipients

--- src/components/determineRecipients.test.js
+++ src/components/determineRecipients.test.js
@@ -1,4 +1,5 @@
 import { determineRecipients } from './determineRecipients';
+
 test('cleans config of curly quotes', () => {
   expect(
     determineRecipients(
@@ -11,6 +12,20 @@ test('cleans config of curly quotes', () => {
     determineRecipients(
       '{“Spiderman”: [“hero”, “tough”, “smart”, “tall”]}',
       '"hero",tall,weak',
+      'AND',
+    ),
+  ).toBe('');
+  expect(
+    determineRecipients(
+      '{ “Human Torch”: [“hero”, “mutant”, “tough”, “dumb”, “tall”], “Spiderman”: [“hero”, “tough”, “smart”, “tall”], “Kyle”: [“human”, “weak”, “smart”, “short”], “JonJon”: [“human”, “strong”, “smart”, “tall”, “weird”] }',
+      'tall',
+      'OR',
+    ),
+  ).toBe('Human Torch, JonJon, Spiderman');
+  expect(
+    determineRecipients(
+      '{“Spiderman”: [“hero”, “tough”, “smart”, “tall”]}',
+      'tall,weak',
       'AND',
     ),
   ).toBe('');
